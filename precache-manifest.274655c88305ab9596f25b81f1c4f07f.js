1a78c18da8753a1dc055454bace86a6d9fef6a11
Jesús Hernández <jesus.hernandezmoreno@outlook.com>
2020-04-27 11:14:35 -0500

    Updates

---
+++ precache-manifest.274655c88305ab9596f25b81f1c4f07f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7d9994444418e1f8d2921235dfe509",
+    "url": "/coffee-shop-pos-concept/index.html"
+  },
+  {
+    "revision": "a1843db2203f570943ba",
+    "url": "/coffee-shop-pos-concept/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "0fb3e806265b4c6d9b60",
+    "url": "/coffee-shop-pos-concept/static/css/main.adb523ff.chunk.css"
+  },
+  {
+    "revision": "a1843db2203f570943ba",
+    "url": "/coffee-shop-pos-concept/static/js/2.aa972a72.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/coffee-shop-pos-concept/static/js/2.aa972a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb3e806265b4c6d9b60",
+    "url": "/coffee-shop-pos-concept/static/js/main.4e607168.chunk.js"
+  },
+  {
+    "revision": "74e68dbe388ca2006480",
+    "url": "/coffee-shop-pos-concept/static/js/runtime-main.142563f7.js"
+  },
+  {
+    "revision": "f4999f494a6dc56ecfc35830ba9b93f6",
+    "url": "/coffee-shop-pos-concept/static/media/logo.f4999f49.png"
+  }
+]);
